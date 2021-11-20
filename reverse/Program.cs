4332bb8048321b28b55486e271d576cf2e967e37
Kirill Borisenko <kirill.borisenko03@gmail.com>
2021-11-20 17:21:32 +0300

    Добавьте файлы проекта.

---
+++ reverse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str_2 = new string (str.ToCharArray().Reverse().ToArray());
+            Console.WriteLine(str_2);
+            Console.ReadLine();
+        }
+    }
+}
